367a08c32a01f7918d01fec8fe700a6b8d603ca7
Gilvan Almeida <g6almeida@hotmail.com>
2017-06-15 17:43:39 -0300

    Classe controller CRUD filmes.

--- app/controllers/DiretorCRUD.java
+++ app/controllers/DiretorCRUD.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Diretor;
 import play.data.Form;
+import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -24,8 +25,12 @@ public class DiretorCRUD extends Controller{
     }
 
     public static Result remover(Long id) {
-        Diretor.find.ref(id).delete();
-        flash("sucesso","Diretor removido com sucesso");
+        try {
+            Diretor.find.ref(id).delete();
+            flash("sucesso","Diretor removido com sucesso");
+        } catch (Exception e) {
+            flash("erro", Messages.get("global.erro"));
+        }
         return lista();
     }
 
@@ -60,7 +65,7 @@ public class DiretorCRUD extends Controller{
         }
         Diretor diretor = form.get();
         diretor.save();
-        // form.get().save();
+        //ou form.get().save();
         flash("sucesso","Registro gravado com sucesso");
 
         return redirect(routes.DiretorCRUD.lista());
