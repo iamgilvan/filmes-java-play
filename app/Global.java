64beaf0ce88a1e175c38747ceef97ccb13ddbf94
Gilvan Almeida <g6almeida@hotmail.com>
2017-06-15 18:05:32 -0300

    Exemplo para ocultar urls.

---
+++ app/Global.java
@@ -0,0 +1,19 @@
+/*
+import static play.mvc.Results.notFound;
+import play.GlobalSettings;
+import play.libs.F.Promise;
+import play.mvc.Http.RequestHeader;
+import play.api.mvc.Result;
+
+*/
+/**
+ * Created by gilvan on 12/06/17.
+ *//*
+
+public class Global extends GlobalSettings{
+    @Override
+    public Promise<Result> onHandlerNotFound(RequestHeader request){
+        return Promise<Result> pure(notFound(views.html.paginaNaoEncontrada.render(request.uri())));
+    }
+}
+*/
